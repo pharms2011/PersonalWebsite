e56fb7a1238b50fa9e8ecebc478760a385a32073
pharms2011 <prestonharms5@gmail.com>
2018-08-22 16:56:33 -0700

    Some refactoring in the education bean

--- PersonalWebsite/src/main/java/com/inertia/controllers/EducationController.java
+++ PersonalWebsite/src/main/java/com/inertia/controllers/EducationController.java
@@ -21,7 +21,7 @@ import com.inertia.services.EducationServiceImpl;
 
 @RequestMapping("/education")
 @Controller("educationController")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 public class EducationController {
 	
 	@Autowired
